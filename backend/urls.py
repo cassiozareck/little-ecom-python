28a348fa4dab5db8e26c5e8469c5a2a3cfbea3f3
zareck <121526696+cassiozareck@users.noreply.github.com>
2023-12-12 18:55:14 +0000

    buy endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,13 @@
 # urls.py
 
 from django.urls import path
-from .views import remove_item  # Import the view
+from .views import remove_item, add_item, get_items, update_item, buy_item  # Import the view
 
 urlpatterns = [
     
     path('remove-item/<int:item_id>/', remove_item, name='remove_item'),
-    
+    path('add-item/', add_item, name='add_item'),
+    path('items/', get_items, name='get_items'),
+    path('update/', update_item, name='update_item'),
+    path('buy-item', buy_item, name="buy_item"),
 ]
--- backend/views.py
+++ backend/views.py
@@ -25,7 +25,7 @@ def remove_item(request, item_id):
         item = get_object_or_404(Item, pk=item_id)
         if not item.is_owner(owner):
             return HttpResponseBadRequest("You must be the item's owner to delete it.")
-
+        
         item.delete()
         return HttpResponse(status=204)
 
@@ -60,12 +60,6 @@ def add_item(request):
     except KeyError:
         return HttpResponseBadRequest("Missing fields in JSON")
 
-
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
-from django.http import JsonResponse, HttpResponseBadRequest
-from django.shortcuts import get_object_or_404
-import json
 
 @csrf_exempt
 @require_http_methods(["PATCH"])
@@ -105,3 +99,43 @@ def get_items(request):
         # Use a logger in production code
         print(f"Error retrieving items: {e}")
         return HttpResponseServerError("Internal Server Error")
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def buy_item(request):
+    try:
+        # Parse JSON data from the request
+        data = json.loads(request.body)
+        item_id = data.get("id")
+
+        # Authenticate user
+        user_info = getattr(request, 'user_info', None)
+        if not user_info or 'email' not in user_info:
+            return HttpResponseBadRequest("Unauthorized access")
+        buyer = user_info['email'].split('@')[0]
+
+        # Retrieve the item
+        try:
+            item = Item.objects.get(id=item_id)
+        except Item.DoesNotExist:
+            return HttpResponseNotFound("Item not found")
+
+        # Check if the buyer is not the owner of the item
+        if item.owner == buyer:
+            return HttpResponseBadRequest("Cannot buy your own item")
+
+        Item.objects.delete(id=item_id)
+
+        return JsonResponse({'message': 'Item bought successfully', 'item_id': str(item_id)}, status=200)
+
+    except json.JSONDecodeError:
+        return HttpResponseBadRequest("Invalid JSON")
+    except Exception as e:
+        # Log the exception for debugging purposes
+        # Use a logger in production code
+        print(f"Error in buying item: {e}")
+        return HttpResponseServerError("Internal Server Error")
+
+
+        
